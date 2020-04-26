596b0e330688029dc49edbb63a1b474114f7dc26
Li-13 <64051316+Li-13@users.noreply.github.com>
2020-04-26 20:32:39 +0800

    Add files via upload

---
+++ EasyWebApplication.java
@@ -0,0 +1,14 @@
+package com.zyy;
+
+import com.zyy.oauth.configuration.EnableOAuthClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableOAuthClient
+@SpringBootApplication
+public class EasyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyWebApplication.class, args);
+    }
+}
