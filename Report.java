5d84d3fe5877a9108653754a12648fe384538915
huo129 <64052577+huo129@users.noreply.github.com>
2020-05-03 14:53:01 +0800

    Add files via upload

---
+++ Report.java
@@ -0,0 +1,43 @@
+package com.zyy.system.model;
+
+import java.math.BigDecimal;
+
+public class Report {
+    private String beginTime;
+    private String endTime;
+
+    private String name;
+    private BigDecimal account;
+
+    public String getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(String beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getAccount() {
+        return account;
+    }
+
+    public void setAccount(BigDecimal account) {
+        this.account = account;
+    }
+}
